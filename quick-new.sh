951a77ae50e4d66bce556e17e2161a6d9d5fe588
Trung Vu <Trung.Vu@warwick.ac.uk>
2025-07-25 15:39:29 +0300

    Updates quick-new.sh for extra functionalities (see description)
    The script now understands 3 letters input e.g. if you enter only "SMI" in the input, it will automatically work out a person code based on that. Compared to before, this saves the two keystrokes needed for the number part of the person ID.
    After the new file is created, the script will attempt to open said new file with Oxygen, saving the time and effort spent on manually searching and opening that file. The opening itself might take a while. That's just Oxygen donig its thing.
    Finally, the script is a tiny bit more readable for those interesting in the nuts and bolts :)
    
    As always, feel free to email me for any questions you may have.

--- quick-new.sh
+++ quick-new.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cd "`dirname "$0"`"
+# Function defined start
+
+fancyelip () {
+    if [ "$1" == "" ]; then elipdelay="0.1"; else elipdelay="$1"; fi
+    for ((n=0;n<3;n++)); do sleep "$elipdelay" && echo -n "."; done
+}
+
+# Function defined end
+
+[ "$(uname -s)" == "*Darwin*" ] && echo "Current operating system is OSX. This script is completely untested on OSX and you may encounter wild and fantastical errors. Proceed at your own risk."
+
+cd "$(dirname "$0")" || exit 1
 
 templatefilename="AApersonTemplate.xml"
 
@@ -15,26 +26,32 @@ fi
 
 if [ "$1" == "" ]; then
     clistart=0
-    read -p "Enter person ID: " nameorid # or full path, quod libet
+    read -p "Enter person ID: " nameorid # ID or three letters or full path, quod libet
     work="$nameorid"
 else
     clistart=1
     work="$1"
 fi
 
-templatefile=$(find -type f -path "*/$templatefilename" -prune)
-
+templatefile=$(find . -type f -path "*/$templatefilename" -prune) # TODO this assumes only one templatefile with the templatefilename... That's gotta be solved somehow
 echo -ne "Template file in use: \033[1;35m$templatefile\033[0m.\n"
 
-if [[ "$PWD" == *"test1" ]] && [[ "$work" == ????? ]] && [[ "$work" != ?????".xml" ]]; then # check dir, check input to see if it's a persid or something else
-    newfile="./people/$work.xml"
+if [[ "$work" == ????? ]] && [[ "$work" != ?????".xml" ]]; then # checks input to see if it's a persid. three letters only, or full path
+    if [[ "$PWD" == *"test1" ]]; then newfile="./people/$work.xml"
+    elif [[ "$PWD" == *"people" ]]; then newfile="./$work.xml"
+    fi
     persid="$work"
-elif [[ "$PWD" == *"people" ]] && [[ "$work" == ????? ]] && [[ "$work" != ?????".xml" ]]; then # above
-    newfile="./$work.xml" # this is like this because ideally you would use the shell's autocomplete function to check if a name is taken, and thus write ./people/XXX99.xml if you're in test1/ or ./XXX99.xml if you're in people/
-    persid="$work"
-else
-    newfile="$work"
+elif [[ "$work" == ??? ]] && [[ "$work" =~ ^[A-Z]+$ ]]; then
+    if [[ "$PWD" == *"test1" ]]; then newfile="./people/$work""01.xml"
+    elif [[ "$PWD" == *"people" ]]; then newfile="./$work""01.xml"
+    fi
+    persid="$work""01"
+elif [[ "$PWD" == *"people" ]] || [[ "$PWD" == *"test1" ]]; then # below is for backwards compatibility with primitive workflow that deals with raw file paths; kinda bork
+    newfile="$work" # this is like this because ideally you would use the shell's autocomplete function to check if a name is taken, and thus write ./people/XXX99.xml if you're in test1/ or ./XXX99.xml if you're in people/
     persid=${newfile%.xml} && persid=${persid##*/} # extracts the code id from the filename, since they are one in the same.
+else
+    echo "Something's wrong."
+    exit 1
 fi
 
 if [[ ${newfile: -4} == ".xml" ]]; then
@@ -44,29 +61,33 @@ else
     newfile="$newfile.xml"
 fi
 
-echo -ne "Current person ID value: \033[0;32m$persid\033[0m.\nChecking filename validity" && for ((n=0;n<3;n++)); do sleep 0.2 && echo -n "."; done; echo -ne " " # Literally cosmetic and pointless, but it looks cool though.
-if [[ ${newfile: -9} == ?????".xml" ]] && [ "${persid: -2}" -le "99" ] && [ "${persid: -2}" -ge "0" ]; then # Checks if filename has exactly 5 characters then checks if the last two characters are numbers between 0-99
+echo -ne "Current person ID value: \033[0;32m$persid\033[0m.\nChecking filename validity" && fancyelip; echo -ne " " # Literally cosmetic and pointless, but it looks cool though.
+
+if [[ ${newfile: -9} == ?????".xml" ]] && [[ "${persid:3:2}" -le "99" ]] && [[ "${persid:3:2}" -ge "1" ]]; then # Checks if filename has exactly 5 characters then checks if the last two characters are numbers between 1-99 inclusive
     echo "Filename seems to follow the 5 char convention!"
 else
     echo "That name doesn't quite seem right. Try again?"
+    sleep 10
     exit 1
 fi
 
 new=${persid: -2}
 
-if [ -f $newfile ]; then
+if [ -f "$newfile" ]; then
     echo -ne "\033[0;31mFile already exists\033[0m! Attempting to fix filename to not conflict. "
     if [[ "$PWD" == *"test1" ]]; then
-        if [[ ! $(uname -o) == "GNU/Linux" ]]; then echo "If you're stuck on this, please be patient let it run its course. This can occur the first time the script used after starting the operating system."; fi
-        cur=$(ls ./people | grep "${persid:0:3}..\.xml" | tail -n 1)
+        if [[ ! $(uname -o) == "GNU/Linux" ]]; then echo "If you're stuck on this, please be patient and let it run its course. This can occur the first time the script used after starting the operating system."; fi
+        switch2="people/"
     elif [[ "$PWD" == *"test1/people" ]]; then
-        cur=$(ls ./ | grep "${persid:0:3}..\.xml" | tail -n 1)
+        switch2=""
     else
         echo "You're in the wrong directory. How is that possible? We checked above!"
+        exit 1
     fi
 
-    cur=${cur%.xml} && cur=${cur##*/}; cur=${cur: -2}; cur=$((10#0$cur)) # grabs current
-    new=$((10#$(($new+$(($cur-$new))+1))))
+    cur=$(find ./$switch2 -maxdepth 1 -path "*${persid:0:3}*xml" | sort | tail -n 1)
+    cur=${cur%.xml} && cur=${cur##*/}; cur=${cur: -2}; cur=$((10#0$cur)) # grabs current persid
+    new=$((10#$((new+$((cur-new))+1))))
 
     if [ $new -le "9" ]; then new="0$new"; fi
     persid="${persid:0:3}$new"
@@ -76,26 +97,46 @@ if [ -f $newfile ]; then
         newfile="./people/$persid.xml"
     fi
     echo -e "New person ID: \033[0;32m$persid\033[0m."
+    if [ -f "$newfile" ]; then echo "We've failed." && sleep 3 && exit 1; fi
 fi
+
 #
-# Error checking done, actual work starts here
+# Error checking and prelude done, actual work starts here
 #
+
 echo -e "Creating \033[0;32m$newfile\033[0m."
-cp --no-clobber $templatefile $newfile || echo "Something is still wrong."
 
-echo "Echoing in person ID and formatting XML file with sed."
-sed -i "s/xml:id=\"XXX99\"/xml:id=\"$persid\"/g" $newfile
-if [ ! "$2" == "" ]; then sed -i "s/sex=\"1\"/sex=\"$2\"/g" $newfile; fi
+cp --no-clobber "$templatefile" "$newfile" || echo "Something is still wrong."
 
-if [ $(uname -o) == "GNU/Linux" ] && ( command -v kate >/dev/null 2>&1 ); then # this is my personal thing, don't worry about it!
-    kate "$newfile"
+echo "Writing in person ID and formatting XML file with sed."
+sed -i "s/xml:id=\"XXX99\"/xml:id=\"$persid\"/g" "$newfile"
+[ ! "$2" == "" ] && sed -i "s/sex=\"1\"/sex=\"$2\"/g" "$newfile"
+
+if [ "$(uname -o)" == "GNU/Linux" ] && ( command -v kate >/dev/null 2>&1 ); then # this is my personal thing, don't worry about it!
+    echo "Opening with kate." && kate "$newfile"
+elif [ "$(uname -o)" == "Msys" ]; then # automatically opens up Oxygen for editing. A bit slow but that's on the part of Oxygen.
+    oxxmlloc="/c/Program Files/Oxygen XML Editor 27/oxygen.bat"
+    if [ -f "$oxxmlloc" ]; then
+        "$oxxmlloc" file:"$newfile" 2>/dev/null &
+        echo -n "Found Oxygen XML Editor. Opening now. Please wait a few seconds" && fancyelip "0.3"; echo ""
+    elif [ ! -f "$oxxmlloc" ]; then
+        echo "Did not find Oxygen XML Editor at expected location. Trying again..."
+        curveball="$(find "/c/Program Files/" -maxdepth 2 -path "*oxygen.bat" 2>/dev/null)"
+        [ ! "$curveball" == "" ] && ( "$curveball" file:"$newfile" 2>/dev/null & )
+    fi
+# else
+#     echo "I don't know what OS you're running. So I'm just going to try to open a text editor. Good luck."
+#     editor "$newfile" || nano "$newfile"
 fi
+
 if [ $clistart == 0 ]; then
     echo "Script will close in 10 seconds. To close immediately, press Control + C."
     sleep 10
 fi
 
-# ================ Archived code that exists for no particular reason
+exit 0
+
+# ================ Archived code that exists for no particular reason ================
 # echo "You're probably in the right directory, and the template file is probably available."
 # Dependency check: xmlstarlet; below works maybe
 # if ( ! type "xmlstarlet" > /dev/null ) && ( ! type "xml" > /dev/null ) ; then
@@ -139,3 +180,5 @@ fi
 #             newfile="./people/$persid.xml"
 #         fi
 #     done
+# cur=$(ls --ignore=*html ./ | grep "${persid:0:3}..\.xml" | tail -n 1)
+# cur=$(ls --ignore=*html ./people/${persid:0:3}*xml | tail -n 1)
